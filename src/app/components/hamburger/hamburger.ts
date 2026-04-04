4f080625c3c92c7340ba4220d92bd7d776161ccc
Ivan <ivancoder88@gmail.com>
2026-04-04 18:06:09 +0200

    refactor(homepage): use cleaner naming convetions in Hamburger component

--- src/app/components/hamburger/hamburger.ts
+++ src/app/components/hamburger/hamburger.ts
@@ -2,17 +2,16 @@ import { Component, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hamburger',
-  imports: [],
   templateUrl: './hamburger.html',
   styleUrl: './hamburger.css',
   standalone: true,
 })
 export class Hamburger {
-  iconToggle = signal(false);
+  isActive = signal(false);
   active = output<boolean>();
 
-  onHamburgerClick() {
-    this.iconToggle.set(!this.iconToggle());
-    this.active.emit(this.iconToggle());
+  toggle(): void {
+    this.isActive.update((prev) => !prev);
+    this.active.emit(this.isActive());
   }
 }
