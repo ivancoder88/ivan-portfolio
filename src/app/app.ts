3075d8ff7d86e10a851fcd621f8895ac9eac1668
Ivan <ivancoder88@gmail.com>
2026-04-06 14:02:09 +0200

    refactor(homepage): move components and services into homepage folder and update references

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Navbar } from './components/navbar/navbar';
-import { Hero } from './components/hero/hero';
-import { Services } from './components/services/services';
-import { Works } from './components/works/works';
-import { Resume } from './components/resume/resume';
-import { Skills } from './components/skills/skills';
-import { Testimonials } from './components/testimonials/testimonials';
-import { Contact } from './components/contact/contact';
-import { Footer } from './components/footer/footer';
+import { Navbar } from './homepage/components/navbar/navbar';
+import { Hero } from './homepage/components/hero/hero';
+import { Services } from './homepage/components/services/services';
+import { Works } from './homepage/components/works/works';
+import { Resume } from './homepage/components/resume/resume';
+import { Skills } from './homepage/components/skills/skills';
+import { Testimonials } from './homepage/components/testimonials/testimonials';
+import { Contact } from './homepage/components/contact/contact';
+import { Footer } from './homepage/components/footer/footer';
 
 @Component({
   selector: 'app-root',
